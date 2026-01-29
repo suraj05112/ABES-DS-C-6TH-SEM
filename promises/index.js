cc03607e0a4643c0ced98dc7376accf9ec2c5cfc
Suraj Yadav <surajyadav@gmail.com>
2026-01-29 13:51:53 +0530

    first commit

---
+++ promises/index.js
@@ -0,0 +1,31 @@
+const mypromise = new Promise((resolve, reject)=>{
+let age = 25;
+if(age > 18){
+    resolve("You are eligible to vote");
+} else{
+    reject("You are not eligible to vote");
+}
+});
+
+// mypromise.then((message)=>{             
+//     console.log(message);
+// })
+// .catch((message)=>{
+//     console.log(message);
+// })
+
+const f1 = async ()=>{
+    const message = await mypromise;
+    console.log(message);
+}
+f1();
+
+const getData = async()=>{
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();  
+    return data;
+
+}
+getData().then((data)=>{
+    console.log(data);
+});
